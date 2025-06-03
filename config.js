e80bf6709c4e41b17813ad97788e083e84625167
Broken-byte-dot <Sihimd165@gmail.com>
2025-06-03 18:37:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'MOVIE-VISPER=np5nQaSI#k5IITAid5zvugwJDm0AIx2ESbQXUN2TkIDngyj_jHGo' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'MOVIE-VISPER=ShBHGZaL#5QIsH8oj3DRp4LZqtH1dL_XxDcV4RBPSjlzV8ASo7UU' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
 
